705fdfb1aa319a2c8bdeb5aa81bc04656b91dde1
Phil Stanhope <pstanhope@dyn.com>
2013-10-30 13:29:43 -0400

    automatically create iterators @ runtime

--- InsertDnsLogWithBatchWriter.java
+++ InsertDnsLogWithBatchWriter.java
@@ -40,10 +40,11 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
-import org.apache.accumulo.core.iterators.Combiner;
-import org.apache.accumulo.core.iterators.LongCombiner;
-import org.apache.accumulo.core.iterators.TypedValueCombiner.Encoder;
+// import org.apache.accumulo.core.iterators.Combiner;
+import org.apache.accumulo.core.client.IteratorSetting;
 import org.apache.accumulo.core.iterators.user.SummingCombiner;
+import java.util.Collections;
+import java.util.Iterator;
 
 // guava-15.0.jar 
 // https://code.google.com/p/guava-libraries/
@@ -112,50 +113,45 @@ public class InsertDnsLogWithBatchWriter {
 						  TableNotFoundException {
 	if (args.length < 6) {
 	    System.out
-		.println("Usage: accumulo examples-simplejar accumulo.examples.helloworld.InsertDnsLogWithBatchWriter <instance name> <zoo keepers> <username> <password> <tableName> <logFile> <rowkey> <start> <maxrecords>");
+		.println("Usage: accumulo examples-simplejar accumulo.examples.helloworld.InsertDnsLogWithBatchWriter <instance name> <zoo keepers> <username> <password> <tableName> <logFile> <rowkey> <start> <maxrecords> <rrtype_details>");
 	    System.exit(1);
 	}
     
-	String instanceName = args[0];
-	String zooKeepers = args[1];
-	String user = args[2];
-	byte[] pass = args[3].getBytes();
-	String tableName = args[4];
+	final String instanceName = args[0];
+	final String zooKeepers = args[1];
+	final String user = args[2];
+	final byte[] pass = args[3].getBytes();
+	final String tableName = args[4];
 	String records = "1000000";
 	String rowkey = "1382375700";
 	String logFile = "1382375700";
 	int maxRecords = 0;
+	boolean rrtype_details = false;
 
 	if (args.length > 5) {
 	    logFile = args[5];
+	}
 
 	if (args.length > 6) {
 	    rowkey = args[6];
+	}
 
 	int start = 0;
 	if (args.length > 7) {
 	    try {
-		start = Integer.parseInt(args[6]);
+		start = Integer.parseInt(args[7]);
 	    } catch(Exception e) {
 		System.out.println("WARN: Invalid <START>, assuming 0");
 	    }
 	}
 
 	String maxrecords = "1000000";
-	if (args.length > 7)
-	    maxrecords = args[7];
+	if (args.length > 8)
+	    maxrecords = args[8];
+
+	if (args.length > 9)
+	    rrtype_details = args[9].equals("true");
     
-	ZooKeeperInstance instance = new ZooKeeperInstance(instanceName, zooKeepers);
-	Connector connector = instance.getConnector(user, pass);
-	MultiTableBatchWriter mtbw = connector.createMultiTableBatchWriter(200000l, 300, 4);
-    
-	BatchWriter bw = null;
-    
-	if (!connector.tableOperations().exists(tableName))
-	    connector.tableOperations().create(tableName);
-	bw = mtbw.getBatchWriter(tableName);
-    
-	BufferedReader in = null;
 	Integer val = null;
 	try {
 	    val = Integer.parseInt(maxrecords);
@@ -163,32 +159,73 @@ public class InsertDnsLogWithBatchWriter {
 	if (val != null) {
 	    maxRecords = val;
 	}
-	System.out.printf("maxRecords %d\n", maxRecords);
-	Value one = new Value("1".getBytes());
-	Text r_query = new Text("query");
-	Text r_a = new Text("A");
-	Text r_aaaa = new Text("AAAA");
-	Text r_ptr = new Text("PTR");
-	Text r_txt = new Text("TXT");
-	Text r_srv = new Text("SRV");
-	Text r_soa = new Text("SOA");
-	Text r_spf = new Text("SPF");
-	Text r_mx = new Text("MX");
-	Text r_any = new Text("ANY");
-	Text r_ds = new Text("DS");
-	Text r_a6 = new Text("A6");
-	Text r_rrsig = new Text("RRSIG");
-	Text r_dnskey = new Text("DNSKEY");
-	Text r_ns = new Text("NS");
-	Text r_cname = new Text("CNAME");
-	Text r_naptr = new Text("NAPTR");
-	Text r_tkey = new Text("TKEY");
-	Text r_sshfp = new Text("SSHFP");
-	Text r_hinfo = new Text("HINFO");
-	Text r_fqdns = new Text("fqdns");
-	Text r_clientip = new Text("clientip");
-	Text r_error = new Text("error");
 
+	System.out.printf("logFile    => %s\nrowKey     => %s\nstart      => %d\nmaxRecords => %d\nrrtypes    => %b\n", logFile, rowkey, start, maxRecords, rrtype_details);
+
+	final ZooKeeperInstance instance = new ZooKeeperInstance(instanceName, zooKeepers);
+	final Connector connector = instance.getConnector(user, pass);
+	final MultiTableBatchWriter mtbw = connector.createMultiTableBatchWriter(200000l, 300, 4);
+    
+	BatchWriter bw = null;
+    
+	// Create table
+	if (!connector.tableOperations().exists(tableName)) {
+
+	    // Create the table
+	    connector.tableOperations().create(tableName);
+	    System.out.printf("Added table %s\n", tableName);
+
+	    // Accumulate overall counts
+	    final IteratorSetting iterCounts = new IteratorSetting (1, "counts", SummingCombiner.class);
+	    SummingCombiner.setColumns(iterCounts, Collections.singletonList(new IteratorSetting.Column("counts")));
+	    SummingCombiner.setEncodingType(iterCounts, SummingCombiner.Type.STRING);
+	    connector.tableOperations().attachIterator(tableName, iterCounts);
+	    System.out.printf("Added SummingCombiner for 'counts'\n");
+
+	    // Accumulate overall per zone counts
+	    final IteratorSetting iterZones = new IteratorSetting (11, "zones", SummingCombiner.class);
+	    SummingCombiner.setColumns(iterZones, Collections.singletonList(new IteratorSetting.Column("zones")));
+	    SummingCombiner.setEncodingType(iterZones, SummingCombiner.Type.STRING);
+	    connector.tableOperations().attachIterator(tableName, iterZones);
+	    System.out.printf("Added SummingCombiner for 'zones'\n");
+
+	    // Accumulate overall per client counts
+	    final IteratorSetting iterClients = new IteratorSetting (11, "clients", SummingCombiner.class);
+	    SummingCombiner.setColumns(iterClients, Collections.singletonList(new IteratorSetting.Column("clients")));
+	    SummingCombiner.setEncodingType(iterClients, SummingCombiner.Type.STRING);
+	    connector.tableOperations().attachIterator(tableName, iterClients);
+	    System.out.printf("Added SummingCombiner for 'clients'\n");
+
+	}
+	
+	bw = mtbw.getBatchWriter(tableName);
+    
+	BufferedReader in = null;
+	final Value one = new Value("1".getBytes());
+	final Text r_query = new Text("query");
+	final Text r_a = new Text("A");
+	final Text r_aaaa = new Text("AAAA");
+	final Text r_ptr = new Text("PTR");
+	final Text r_txt = new Text("TXT");
+	final Text r_srv = new Text("SRV");
+	final Text r_soa = new Text("SOA");
+	final Text r_spf = new Text("SPF");
+	final Text r_mx = new Text("MX");
+	final Text r_any = new Text("ANY");
+	final Text r_ds = new Text("DS");
+	final Text r_a6 = new Text("A6");
+	final Text r_rrsig = new Text("RRSIG");
+	final Text r_dnskey = new Text("DNSKEY");
+	final Text r_ns = new Text("NS");
+	final Text r_cname = new Text("CNAME");
+	final Text r_naptr = new Text("NAPTR");
+	final Text r_tkey = new Text("TKEY");
+	final Text r_sshfp = new Text("SSHFP");
+	final Text r_hinfo = new Text("HINFO");
+	final Text r_fqdns = new Text("fqdns");
+	final Text r_clientip = new Text("clientip");
+	final Text r_error = new Text("error");
+	
 	HashMap uniqueFQDN = new HashMap();
 	HashMap uniqueClient = new HashMap();
 	Integer errors = 0;
@@ -197,10 +234,27 @@ public class InsertDnsLogWithBatchWriter {
 	int ignores = 0;
 	int cracked = 0;
 
+	// Count & Remember unique rrtypes and top level statistics that we encounter.
+	final Text colf_counts = new Text("counts");
+
+	// Count & Remember unique zones that we encounter.
+	final Text colf_zones = new Text("zones");
+
+	// Count & Remember details per zones that we encounter.
+	final Text colf_details = new Text("details");
+	
+	// Count & Remember details per zones that we encounter.
+	final Text colf_clients = new Text("clients");
+
+	// Timestamp is always our rowkey. We store the node that we found things in 
+	final Text row_counts = new Text(rowkey);
+
+	String node = null;
+
 	try {
 	    in = new BufferedReader(new FileReader(logFile));
 	    while (in.ready()) {
-		String logLine = in.readLine();
+		final String logLine = in.readLine();
 		counter++;
 		if (counter-start >= maxRecords) {
 		    ignores++;
@@ -224,26 +278,23 @@ public class InsertDnsLogWithBatchWriter {
 			// 11 A 
 			// 12 - 
 			// 13 (204.13.250.29)
-			String node = parts[3];
+			node = parts[3];
 			String client = parts[6];
 			String fqdn = stripChars(parts[7],"():'");
 			String query = parts[9] + " " + parts[10] + " " + parts[11];
 			String rrtype = parts[11];
 			String answer = parts[13];
-			//		    System.out.printf("%10d Node=%s Client=%s FQDN=%s RRType=%s Answer=%s\n", counter, node, client, fqdn, rrtype, answer);
+			// System.out.printf("%10d Node=%s Client=%s FQDN=%s RRType=%s Answer=%s\n", counter, node, client, fqdn, rrtype, answer);
 
-			Text row_counts = new Text(rowkey);
 			Mutation m = new Mutation(row_counts);
-
-			// Count & Remember unique FQDNs that we encounter in a FQDNs column family (one column per FQDN)
-			Text colf_fqdns = new Text("FQDNs");
-			Text colf_counts = new Text("counts");
-			Text colf_zones = new Text("zones");
-			Text colf_details = new Text("details");
 
 			// Track and remember counts for each unique record type that we answer for
 			// All RRType Counter
 			m.put(colf_counts, r_query, one);
+
+			// Remember the node that we processed this record for
+			m.put(colf_counts, new Text(node), one);
+
 
 			// Remember domain (in a TLD)
 			String tld = getTopLevelDomain(fqdn, logLine);
@@ -266,17 +317,18 @@ public class InsertDnsLogWithBatchWriter {
 				    m.put(colf_counts, r_clientip, one);
 				}
 
-				// Remember per rrtype per fqdn counts (N rrtype columns)
-				m.put(colf_details, new Text(String.format("%s_%s", tld, rrtype)), one);
+				// Count total per clientip
+				m.put(colf_clients, r_clientip, one);
 
-				// Text row_details = new Text(String.format("%s_%s", rowkey, fqdn));
-				// Mutation mDetails = new Mutation(row_details);
-				// Count & Remember unique details that we encounter in a FQDNs column family (one column per FQDN)
-				// mDetails.put(colf_details, new Text("client"), new Value(client.getBytes()));
-				// mDetails.put(colf_details, new Text("fqdn"), new Value(fqdn.getBytes()));
-				// mDetails.put(colf_details, new Text("reply"), new Value(answer.getBytes()));
-				// mDetails.put(colf_fqdn, new Text("rrtype"), new Value(rrtype.getBytes()));
-				// bw.addMutation(mDetails);
+				// Remember per rrtype per fqdn counts (N rrtype columns)
+				if (rrtype_details) {
+				    m.put(colf_details, new Text(String.format("%s_%s", tld, rrtype)), one);
+				    // Count & Remember unique details that we encounter in per 
+				    // mDetails.put(colf_details, new Text("client"), new Value(client.getBytes()));
+				    // mDetails.put(colf_details, new Text("fqdn"), new Value(fqdn.getBytes()));
+				    // mDetails.put(colf_details, new Text("reply"), new Value(answer.getBytes()));
+				    // mDetails.put(colf_details, new Text("rrtype"), new Value(rrtype.getBytes()));
+				}
 
 				// A Record Counter
 				if (rrtype.equals("A")) {
@@ -390,6 +442,13 @@ public class InsertDnsLogWithBatchWriter {
 	    System.out.printf("Errors  %d\n", errors);
 	    System.out.printf("FQDNs   %d\n", uniqueFQDN.size());
 	    System.out.printf("Client  %s\n", uniqueClient.size());
+
+	    /*
+	    Mutation m = new Mutation(row_counts);
+	    m.put(colf_counts, new Text(node), new Value(ByteBuffer.allocate(4).putInt(cracked).array()));
+	    bw.addMutation(m);
+	    */
+
 	    if (in != null) { try { in.close(); } catch(Throwable t) { /* ensure close happens */ } }
 	}
 
